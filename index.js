48e11c86a39430f367ccc18ae78c0e5c279454e4
Ruben Gil <gil.r419@gmail.com>
2016-12-05 12:19:41 -0500

    added album to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Require models
+const Album = require('./models/album-model');
 const Artist = require('./models/artist-model');
 const Genre = require('./models/genre-model');
 const Playlist = require('./models/playlist-model');
 const Song = require('./models/song-model');
 
 // Require routes
+const albums = require('./routes/album-router.js')
 const artists = require('./routes/artist-router.js');
 const genres = require('./routes/genre-router.js');
 const playlists = require('./routes/playlist-router.js');
@@ -12,12 +14,14 @@ const songs = require('./routes/song-router.js');
 
 module.exports = {
   models: {
+    Album: Album,
     Artist: Artist,
     Genre: Genre,
     Playlist: Playlist,
     Song: Song,
   },
   routes: {
+    albums: albums,
     artists: artists,
     genres: genres,
     playlists: playlists,
